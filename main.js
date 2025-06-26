b4a531138f9024a26a73b4e72d8ccbe1db69fc3f
Trần Xuân Lâm <lamtx@dc1.gemvietnam.com>
2025-06-26 17:11:16 +0700

    update ui

--- main.js
+++ main.js
@@ -2,32 +2,42 @@ function handleSidebarToggle() {
   if (hamburgerMenu.classList.contains("active")) {
     hamburgerMenu.classList.remove("active");
     hamburgerMenu.classList.add("collapsed");
+    setTimeout(() => {
+      overlay.style.display = "none";
+    }, 300);
   } else {
-    mainSidebar.classList.add("active");
-    mainSidebar.classList.remove("collapsed");
+    overlay.style.display = "block";
+    setTimeout(() => {
+      hamburgerMenu.classList.remove("collapsed");
+      hamburgerMenu.classList.add("active");
+    }, 250);
   }
 }
 
 // Toggle sidebar cho desktop
-const hamburgerMenu = document.querySelector(".main-sidebar");
-const hamburgerMenuContent = document.querySelector(".main-sidebar__content");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const hamburgerMenuList = document.querySelector(".hamburger-menu__list");
+const overlay = document.querySelector(".overlay");
 const toggleSidebarBtn = document.querySelector(".header__hamburger-btn");
-const closeSidebarBtn = document.querySelector(".close-sidebar-btn");
+console.log("check ", hamburgerMenu.classList);
+
+const hamburgerMenuClose = document.querySelector(".hamburger-menu__close");
 toggleSidebarBtn.addEventListener("click", handleSidebarToggle);
-// closeSidebarBtn.addEventListener("click", handleSidebarToggle);
+hamburgerMenuClose.addEventListener("click", handleSidebarToggle);
 // Toggle sidebar cho mobile/tablet
 
-document.addEventListener("click", function (e) {
+overlay.addEventListener("click", function (e) {
   if (hamburgerMenu.classList.contains("active")) {
     if (!hamburgerMenu.contains(e.target) && e.target !== toggleSidebarBtn) {
       hamburgerMenu.classList.remove("active");
       hamburgerMenu.classList.add("collapsed");
-      hamburgerMenuContent.classList.add("hidden");
+      setTimeout(() => {
+        overlay.style.display = "none";
+      }, 250);
     }
   }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  fetchData(1);
   hamburgerMenu.classList.add("collapsed");
 });
