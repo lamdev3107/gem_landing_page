7f562b09d6ceb5f610e03b957ee64d8e7ff2997a
Trần Xuân Lâm <lamtx@dc1.gemvietnam.com>
2025-07-01 09:48:16 +0700

    fix:fix hero slider

--- main.js
+++ main.js
@@ -70,11 +70,11 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Tự động chuyển slide mỗi 5 giây
-  let currentSlide = 0;
-  setInterval(() => {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-  }, 3000);
+  // let currentSlide = 0;
+  // setInterval(() => {
+  //   currentSlide = (currentSlide + 1) % slides.length;
+  //   showSlide(currentSlide);
+  // }, 3000);
   // Hiển thị slide đầu tiên khi load
   showSlide(0);
 });
