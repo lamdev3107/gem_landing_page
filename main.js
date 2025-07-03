f8bc18f322c1510dd9200c21fc6cc4038602830b
lamdev3107 <lamtx.3107@gmail.com>
2025-07-03 09:15:12 +0700

    refactor: update class

--- main.js
+++ main.js
@@ -1,44 +1,34 @@
-function handleSidebarToggle() {
-  if (hamburgerMenu.classList.contains("active")) {
-    hamburgerMenu.classList.remove("active");
-    hamburgerMenu.classList.add("collapsed");
-    setTimeout(() => {
-      overlay.style.display = "none";
-    }, 300);
-  } else {
-    overlay.style.display = "block";
-    setTimeout(() => {
-      hamburgerMenu.classList.remove("collapsed");
-      hamburgerMenu.classList.add("active");
-    }, 250);
-  }
-}
-
-// Toggle sidebar cho desktop
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const hamburgerMenuList = document.querySelector(".hamburger-menu__list");
-const overlay = document.querySelector(".overlay");
-const toggleSidebarBtn = document.querySelector(".header__hamburger-btn");
-
-const hamburgerMenuClose = document.querySelector(".hamburger-menu__close");
-toggleSidebarBtn.addEventListener("click", handleSidebarToggle);
-hamburgerMenuClose.addEventListener("click", handleSidebarToggle);
-// Toggle sidebar cho mobile/tablet
-
-overlay.addEventListener("click", function (e) {
-  if (hamburgerMenu.classList.contains("active")) {
-    if (!hamburgerMenu.contains(e.target) && e.target !== toggleSidebarBtn) {
-      hamburgerMenu.classList.remove("active");
-      hamburgerMenu.classList.add("collapsed");
+document.addEventListener("DOMContentLoaded", function () {
+  // Hamburger menu state
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const overlay = document.querySelector(".overlay");
+  const toggleSidebarBtn = document.querySelector(".header__hamburger-btn");
+  const hamburgerMenuClose = document.querySelector(".hamburger-menu__close");
+  function handleSidebarToggle() {
+    if (hamburgerMenu.classList.contains("is-active")) {
+      hamburgerMenu.classList.remove("is-active");
       setTimeout(() => {
         overlay.style.display = "none";
+      }, 300);
+    } else {
+      overlay.style.display = "block";
+      setTimeout(() => {
+        hamburgerMenu.classList.add("is-active");
       }, 250);
     }
   }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  hamburgerMenu.classList.add("collapsed");
+  overlay.addEventListener("click", function (e) {
+    if (hamburgerMenu.classList.contains("is-active")) {
+      if (!hamburgerMenu.contains(e.target) && e.target !== toggleSidebarBtn) {
+        hamburgerMenu.classList.remove("is-active");
+        setTimeout(() => {
+          overlay.style.display = "none";
+        }, 250);
+      }
+    }
+  });
+  toggleSidebarBtn.addEventListener("click", handleSidebarToggle);
+  hamburgerMenuClose.addEventListener("click", handleSidebarToggle);
 
   // Slider Hero
   const slider = document.querySelector(".slider-wrapper");
@@ -56,22 +46,19 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     slider.style.transform = `translateX(${currentSlide * -1 * width}px)`;
     indicators.forEach((dot, i) => {
-      dot.classList.toggle("hero__indicator-item--active", i === currentSlide);
+      dot.classList.toggle(
+        "hero__indicator-item--is-active",
+        i === currentSlide
+      );
     });
   }
-
-  // Khởi tạo slider
-
   updateSlider();
-
   indicators.forEach((dot, i) => {
     dot.addEventListener("click", () => {
       currentSlide = i;
       updateSlider(currentSlide);
     });
   });
-
-  // Tự động chuyển slide mỗi 5s
   setInterval(() => {
     currentSlide = (currentSlide + 1) % totalSlides;
     updateSlider(currentSlide);
